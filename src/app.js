3dd217ae2a1fd13216287d39a1225bed91b1b41c
Suffian <suffianakhtar@gmail.com>
2026-03-30 01:36:39 +0500

    increase file size limit to parsed by express

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(morgan('combined'));
 app.use(cors());
 app.use(helmet());
 app.use(compression());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/images', ImageRouter);
 app.use('/config', ConfigRouter);
